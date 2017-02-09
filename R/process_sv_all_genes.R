5b4e406a1a9895069cee4e2c774160e1e4df0ccb
Olly Burren <ob219@cam.ac.uk>
2017-02-09 16:41:19 +0000

    added code

---
+++ R/process_sv_all_genes.R
@@ -0,0 +1,57 @@
+library(data.table)
+library(GenomicRanges)
+pid.sv.dir<-'/scratch/WGS10K/data/release/20161012-A/merged-sv/all'
+data.dir<-'/scratch/ob219/pid/'
+mfiles<-list.files(path=pid.sv.dir,pattern='filt*.*txt',full.names=TRUE)
+## load in the file of bridge pid cases
+id.file<-fread(file.path(data.dir,'BRIDGE-PID_20161012release_846_index_cases.csv'))
+setnames(id.file,make.names(names(id.file)))
+##CANVAS - insertion deletions based on read depth
+##MANTA - translocations, deletions, tandem duplications, insertions, and inversions based on both paired read fragment spanning and split read evidence
+canvas<-mfiles[grepl("canvas",mfiles)]
+## process canvas
+processCANVAS<-function(f){
+	DT<-fread(f)
+	DT<-subset(DT,ILMN_ID %in% id.file$Illumina.ID)
+	DT$id<-1:nrow(DT)
+	DT
+}
+
+all.canvas<-lapply(canvas,processCANVAS)
+names(all.canvas)<-gsub('\\.txt','',basename(canvas))
+
+load(file.path(data.dir,'hnisz_SE_annotation_all.RData'))
+filterAndAddSEPCHiC<-function(m,sv.gr,sv.dt){
+##load the pchic SE overlaps merge.results.se
+	ol<-as.matrix(findOverlaps(m$se.gr,sv.gr))
+	cbind(sv.dt[ol[,2],],m[ol[,1],c('ensg','name','pchic','se.tissue')])
+}
+
+results.canvas<-lapply(all.canvas,function(sv){
+	sv.gr<-with(sv,GRanges(seqnames=Rle(CANVAS_CHROM),ranges=IRanges(start=CANVAS_START,end=CANVAS_END),id=id))
+	filt.canvas<-lapply(merge.results.se,function(mse)filterAndAddSEPCHiC(mse,sv.gr,sv))
+})
+
+## TODO 
+## checking that the code is doing the correct thing.
+## how to usefully sumarize.
+## what is the background are SV enriched in PID cases vs the other cohorts.
+## how much of SE is duplicated/deleted - what is the GT is it het or hom. what about qualities what about gene deletions as the mmore parsimonious explanation
+
+## prototype downstream analysis on non exonic loss.
+ft<-'canvascalls_allsamples_loss_20161012.ann.filt'
+all.se.ft<-results.canvas[[ft]]
+se.t.types<-sapply(merge.results.se,function(n) sub("\\.csv","",basename(unique(n$se.tissue))))
+rows<-cbind(se.t.types,sapply(all.se.ft,nrow))
+peeps<-cbind(se.t.types,sapply(all.se.ft,function(x) paste(unique(x$BRIDGE_ID),collapse=',')))
+genes<-cbind(se.t.types,sapply(all.se.ft,function(x) paste(unique(sprintf("%s:%s-%s:%s(%s)",x$CANVAS_CHROM,x$CANVAS_START,x$CANVAS_END,x$name,x$BRIDGE_ID)),collapse=',')))
+genes<-cbind(se.t.types,sapply(all.se.ft,function(x) paste(unique(sprintf("%s(%s)",x$name,x$BRIDGE_ID)),collapse=',')))
+table(unlist(strsplit(genes[,2],",")))
+## all genes
+gene_peeps<-unique(unlist(strsplit(genes[,2],",")))
+gper<-data.table(gene=sub("^([^\\(]+).*","\\1",gene_peeps),person=sub("^[^\\(]+\\(([^\\)]+)\\)","\\1",gene_peeps))
+library(ggplot2)
+gper$person<-factor(gper$person,levels=names(sort(table(gper$person),decreasing=TRUE)))
+length(unique(gper$person))
+ggplot(gper,aes(x=person)) + geom_bar() + theme_bw() + theme(axis.text.x=element_text(angle = -90, hjust = 0))
+length(unique(gper$gene))
